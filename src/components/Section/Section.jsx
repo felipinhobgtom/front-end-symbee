9a5b1224ff575ce914405d58dc0e4f17e949eb21
felipinhobgtom <felipinhobgouveia@gmail.com>
2024-10-23 13:49:02 -0300

    esqueci

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Secao() {
   return (
     <section className="section-one">
-      <div className="info outline-yellow-500 outline-dotted outline-8 rounded-full">
+      <div className="info outline-dotted outline-8 rounded-full">
         <h1>Conexões que transformam carreiras!</h1>
         <h3>
           Comunidade freelancer para desenvolvedores brasileiros, que usa a
