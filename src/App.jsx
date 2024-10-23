1b114a79c7d538b03607f2cad723ea4b02bd0d32
felipinhobgtom <felipinhobgouveia@gmail.com>
2024-10-23 09:54:29 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Secao from './components/Section/Section';
+
+function App() {
+  
+  return(
+    <>
+    <p>Nav</p>
+    <Secao />
+    </>
+  )
+}
+
+export default App
