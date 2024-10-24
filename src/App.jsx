2aeda7158b7c8bb319c626a96095f2a75ebaecdb
Luiz Miguel <luizmcestevao@outlook.com>
2024-10-23 21:22:20 -0300

    Nav e Footer

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'; // Importando estilos do App, se houver
+import Footer from './components/footer/Footer';
+import Header from "./components/header/Header"; // Certifique-se de que o caminho está correto
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
