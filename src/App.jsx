34c56764b291472751694b7904e01b794e486a63
Luiz Miguel <luizmcestevao@outlook.com>
2024-10-24 21:43:17 -0300

    luiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css'; // Importando estilos do App, se houver
+import { Outlet } from 'react-router-dom';
 import Footer from './components/footer/Footer';
-import Header from "./components/header/Header"; // Certifique-se de que o caminho está correto
+import NavBar from './components/NavBar/NavBar';// Certifique-se de que o caminho está correto
 
 function App() {
   return (
     <>
-      <Header />
-      <Footer />
+      <NavBar />
+
+      <div>
+      <Outlet />
+      </div>
     </>
   );
 }
