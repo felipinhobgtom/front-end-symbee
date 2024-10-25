29f3ade6102e763c5d8138744316a9773f472c8c
felipinhobgtom <felipinhobgouveia@gmail.com>
2024-10-25 13:09:29 -0300

    section3 + imagens

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Secao from './components/Section/Section';
 import Section4 from './components/Section 4/Section4';
+import Section3 from './components/Section 3/Section3';
 
 function App() {
   
   return(
     <>
     <Secao />
+    <Section3 />
     <Section4 />
     </>
   )
